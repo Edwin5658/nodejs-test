4327d02754ca3059f1cb41792491a912f50fe494
Edwin5658 <edwinchiu618@gmail.com>
2025-07-18 17:52:55 +0800

    ejs

---
+++ app.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const app = express();
+const router = require('./routes/routes');
+
+app.use('/', router);
+
+app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'ejs')
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = 3000;
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error');
+})
+
+app.listen(PORT, () => {
+  console.log('Server running on port 3000');
+});
+
+module.exports = app;     
---
+++ routes/routes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('index');
+});
+
+router.get('/about', (req, res) => {
+    res.render('about');
+});
+
+router.get('/contact-me', (req, res) => {
+    res.render('contact-me');
+});
+
+router.get('/{*splat}', (req, res) => {
+    res.render('404');
+});
+
+module.exports = router;
